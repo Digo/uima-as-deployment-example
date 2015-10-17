61f0cd56d5141ac7336c870056467fe9c3697f7e
Di Wang <kingwangdi@gmail.com>
2015-10-17 12:08:17 -0400

    changed default broker to localhost

--- deployScnlpAs.sh
+++ deployScnlpAs.sh
@@ -3,7 +3,7 @@
 PROJ_FOLDER=`dirname $0`
 
 DD=${1:-src/main/resources/deploy/deploy_scnlp.xml} 
-BROKER=${2:-tcp://mu.lti.cs.cmu.edu:61616}
+BROKER=${2:-tcp://localhost:61616}
 
 export UIMA_JVM_OPTS="-Xms128M -Xmx4000M -XX:-UseGCOverheadLimit"
 export UIMA_HOME=$PROJ_FOLDER/apache-uima-as-2.4.0/
